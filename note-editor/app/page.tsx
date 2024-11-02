8795899f8821b56680c5fa54bebd6499abd8c772
YootTanA <oatyootna@gmail.com>
2024-11-03 01:34:18 +0700

    Add console

--- note-editor/app/page.tsx
+++ note-editor/app/page.tsx
@@ -1,3 +1,9 @@
+import Console from "@/components/Console";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="bg-[#fefced] w-screen h-screen max-w-screen-sm max-h-screen-sm">
+      <Console />
+    </div>
+  );
 }
---
+++ note-editor/components/Console.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import React, { useState, useRef } from "react";
+import { Camera } from "react-camera-pro";
+import Image from "next/image";
+
+interface ConsoleProps {}
+
+const Console: React.FunctionComponent<ConsoleProps> = () => {
+  const camera = useRef(null);
+  const [image, setImage] = useState(null);
+  const [numberOfCameras, setNumberOfCameras] = useState(0);
+
+  const errorMsgs = {
+    noCameraAccessible:
+      "No camera device accessible. Please connect your camera or try a different browser.",
+    permissionDenied:
+      "Permission denied. Please refresh and give camera permission.",
+    switchCamera:
+      "It is not possible to switch camera to different one because there is only one video device accessible.",
+    canvas: "Canvas is not supported.",
+  };
+
+  const [text, setText] = useState("");
+  const maxRows = 2;
+  const maxLength = 50;
+
+  const handleChange = (e) => {
+    const lines = e.target.value.split("\n");
+
+    if (lines.length <= maxRows && e.target.value.length <= maxLength) {
+      setText(e.target.value);
+    } else if (lines.length > maxRows) {
+      setText(lines.slice(0, maxRows).join("\n"));
+    }
+  };
+
+  const handleReset = () => {
+    setImage(null);
+    setText("");
+  };
+
+  return (
+    <div className="px-5">
+      <div className="grid justify-items-center pt-3">
+        <div className="flex flex-col w-[22rem] h-[28rem] rounded-md bg-white shadow-lg">
+          <div className="flex justify-center pt-3">
+            <div className="relative w-[20rem] h-[20rem] rounded-md max-h-sm max-w-sm">
+              {image == null ? (
+                <Camera
+                  ref={camera}
+                  errorMessages={errorMsgs}
+                  numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
+                />
+              ) : (
+                <img src={image} alt="Taken photo" />
+              )}
+            </div>
+          </div>
+          <textarea
+            value={text}
+            rows={maxRows}
+            maxLength={maxLength}
+            onChange={handleChange}
+            placeholder="Write your moment..."
+            className="m-4 text-xl resize-none focus:outline-none focus:ring-0 overflow-hidden"
+          ></textarea>
+        </div>
+      </div>
+
+      <div className="mt-6 grid justify-items-center">
+        <div className="flex space-x-5">
+          <button
+            disabled={numberOfCameras <= 1}
+            onClick={() => {
+              if (camera.current) {
+                const result = camera.current.switchCamera();
+              }
+            }}
+          >
+            <Image src="/switch-camera.svg" width={40} height={40} alt="" />
+          </button>
+          <button
+            className="p-4 shadow-sm bg-white w-20 h-20 rounded-full grid justify-items-center"
+            onClick={() => setImage(camera.current.takePhoto())}
+          >
+            <div className="p-6 bg-red-100 rounded-full" />
+          </button>
+          <button onClick={handleReset}>
+            <Image src="/reset.svg" width={40} height={40} alt="" />
+          </button>
+        </div>
+      </div>
+
+      <button className="mt-4 shadow-md bg-[#f17a7e]  border-b-4 border-[#ec4c51] py-2 px-4 text-white font-bold rounded-md">
+        Post !
+      </button>
+    </div>
+  );
+};
+
+export default Console;
